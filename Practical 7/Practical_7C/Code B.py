43b55ec3cbe8dfcdfa2b017249a484efb2b97ad3
Himani Malankar <himanimalankar@gmail.com>
2026-08-05 19:26:05 +0530

    Create Code B.py

---
+++ Practical 7/Practical_7C/Code B.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# Load Dataset
+data = pd.read_csv("mnist_test.csv")   # Loaded but not used
+digits = load_digits()
+
+# Features and Target
+X = digits.data
+y = digits.target
+
+# Binary Classification (Digit = 7)
+y = (y == 7)
+
+# Split Dataset
+X_train, X_test, y_train, y_test = train_test_split(
+   X, y, test_size=0.3, random_state=1
+)
+
+# Train Model
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+# Accuracy
+print("Training Accuracy :", model.score(X_train, y_train) * 100)
+print("Testing Accuracy  :", model.score(X_test, y_test) * 100)
+
+# Predict First Test Sample
+prediction = model.predict([X_test[0]])
+
+if prediction[0]:
+   print("Digit is 7")
+else:
+   print("Digit is not 7")
